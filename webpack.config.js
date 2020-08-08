a032aededfa9f73d8364f7793f33ff723ad24316
MJunaidK <junaidcts@gmail.com>
2020-08-08 16:50:03 -0400

    Adding loader or rule for babel

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,20 @@ module.exports = {
     output: {
         filename: "dist/bundle.js"
     },
-    watch: true    
+    watch: true,
+    module: {
+        rules: [
+            {
+                test: /\.es6$/, // WHat kind of files to run through these loader
+                exclude: /node_modules/, // exclude node mdules
+                loader: "babel-loader"  // name of the loader
+            }
+        ]
+    },
+    
+    resolve: {
+        extensions: [".js",".es6"]
+        //What kind of file type we can process without specifially gving the file extension.
+        // weback will look for login.js or login.es6 file
+    }
 }
