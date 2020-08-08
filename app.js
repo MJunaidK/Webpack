b6583dea09aae1354731650a652eec99e92696d3
MJunaidK <junaidcts@gmail.com>
2020-08-08 14:44:16 -0400

    Webpack with multiple source files

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
+require('./login.js');
 document.write("Welcome to Big Hair Concerts!");
 console.log('App Loaded with webpack config file in watch mode runnning on webpack dev server!!!');  
+console.log('App Loaded');
