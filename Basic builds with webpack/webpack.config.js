aa26ac8303de411c4fe0ed1bad52a17df6500c7c
MJunaidK <junaidcts@gmail.com>
2020-08-08 21:21:55 -0400

    Basic builds with  webpack module changes

---
+++ Basic builds with webpack/webpack.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    entry: ["./utils.js","./app.js"],
+    output: {
+        filename: "bundle.js"
+    },
+    watch: true,
+    module: {
+        
+        rules: [
+            {
+                test: /\.es6$/, // WHat kind of files to run through these loader
+                exclude: /node_modules/, // exclude node mdules
+                loader: "babel-loader"  // name of the loader
+            },
+            {
+                test: /\.js/,
+                exclude: /node_modules/,
+                loader: 'eslint-loader'
+
+            }
+        ]
+    },
+    
+    resolve: {
+        extensions: [".js",".es6"]
+        //What kind of file type we can process without specifially gving the file extension.
+        // weback will look for login.js or login.es6 file
+    }
+}
